097ef51f49ea63a09e22a5ef3c29d6d55be08483
matunga <matunga@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-04-15 04:17:41 +0000

    Test 1

---
+++ racetrack/grails-app/domain/racetrack/Registration.groovy
@@ -0,0 +1,18 @@
+package racetrack
+
+class Registration {
+
+	String name
+	Date dateOfBirth
+	String gender
+	String address
+	String city
+	String state
+	String zipcode
+	String email
+	Date dateCreated //Note: this is a special name
+	Date lastUpdated
+	
+    static constraints = {
+    }
+}
