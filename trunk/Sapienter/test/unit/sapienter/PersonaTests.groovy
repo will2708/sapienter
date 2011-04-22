ab92504cad347c9891d8e38cbafcaf8e74612b88
matunga <matunga@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-04-22 23:43:18 +0000

    Commit Inicial de Sapienter!

---
+++ trunk/Sapienter/test/unit/sapienter/PersonaTests.groovy
@@ -0,0 +1,19 @@
+package sapienter
+
+import grails.test.*
+
+class PersonaTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testPersona() {
+		def persona = new Persona(nombre:"Matias",apellido:"Toth")
+		assertEquals "Matias", persona.nombre.toString()
+		assertEquals "Toth", persona.apellido.toString()
+    }
+}
