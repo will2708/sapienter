9c417e736f3115c9975341dab099400818021e8b
matunga <matunga@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-08-17 00:39:58 +0000

    Constraints de calendario y correo electrónico

--- Sapienter/grails-app/domain/sapienter/Calendario.groovy
+++ Sapienter/grails-app/domain/sapienter/Calendario.groovy
@@ -1,15 +1,14 @@
 package sapienter
-
+//TODO: Modificar lista en los tipos de tareas
 class Calendario {
 	String descripcion
-//	Tarea tareas
 	String tipoDeTareas
 	Usuario usuarioResponsable
 	
     static constraints = {
-		descripcion()
-		tipoDeTareas()
-		usuarioResponsable()	
+		descripcion(blank:false)
+		tipoDeTareas(inList=["1","2","3","4"])
+		usuarioResponsable(nullable:false)	
     }
 	
 	static hasMany   = [tareas:Tarea]
