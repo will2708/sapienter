38891222aeacccdf2b428992bc4d1871a398001f
matunga <matunga@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-08-27 02:26:13 +0000

    Cambio en el Bootstrap para generar automáticamente un Estudio

--- trunk/Sapienter/grails-app/conf/BootStrap.groovy
+++ trunk/Sapienter/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import sapienter.Estudio
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> 
+		1.times { i ->
+			 new Estudio(razonSocial: "Sapienter", cuit: "20-30409317-0", domicilioFiscal: "Mozart y Zaraza",).save()
+    }
     }
     def destroy = {
     }
