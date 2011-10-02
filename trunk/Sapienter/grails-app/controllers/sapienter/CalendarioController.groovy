c41fe917c3502035e5b5fdfe8d6dd633347ebaad
nicolassouto@gmail.com <nicolassouto@gmail.com@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-10-02 19:41:36 +0000

    Problema de la hora en el calendario solucionado

--- trunk/Sapienter/grails-app/controllers/sapienter/CalendarioController.groovy
+++ trunk/Sapienter/grails-app/controllers/sapienter/CalendarioController.groovy
@@ -2,6 +2,7 @@ package sapienter
 
 import grails.converters.JSON
 
+import java.text.SimpleDateFormat
 import java.util.Date
 
 class CalendarioController {
@@ -119,17 +120,39 @@ class CalendarioController {
 	def events = {
 		def start = new Date(params.long('start')*1000)
 		def end = new Date(params.long('end')*1000)
+		
+		def fecha = new Date()
+		println fecha
 
+		Date prueba2 = this.getDateInTimeZone(fecha, "GMT-3") 
+		println prueba2
+		
 		render Tarea.findAllByFechaInicioBetween(start, end).collect {
 		   [
 				 id: it.id,
 				 title: "$it.observacion",
-				 start: it.fechaInicio,
-				 end: it.fechaFin,
+				 start: this.getDateInTimeZone(it.fechaInicio, "GMT-6"),
+				 end: this.getDateInTimeZone(it.fechaFin, "GMT-6"),
 				 allDay: false,
 				 url: g.createLink(controller:"tarea", action:'show', id:it.id),
 		   ]
 		} as JSON
 	 }
-	
+	public static Date getDateInTimeZone(Date currentDate, String timeZoneId)
+	{
+		TimeZone tz = TimeZone.getTimeZone(timeZoneId);
+		Calendar mbCal = new GregorianCalendar(TimeZone.getTimeZone(timeZoneId));
+		mbCal.setTimeInMillis(currentDate.getTime());
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, mbCal.get(Calendar.YEAR));
+		cal.set(Calendar.MONTH, mbCal.get(Calendar.MONTH));
+		cal.set(Calendar.DAY_OF_MONTH, mbCal.get(Calendar.DAY_OF_MONTH));
+		cal.set(Calendar.HOUR_OF_DAY, mbCal.get(Calendar.HOUR_OF_DAY));
+		cal.set(Calendar.MINUTE, mbCal.get(Calendar.MINUTE));
+		cal.set(Calendar.SECOND, mbCal.get(Calendar.SECOND));
+		cal.set(Calendar.MILLISECOND, mbCal.get(Calendar.MILLISECOND));
+		
+		return cal.getTime();
+	}
 }
