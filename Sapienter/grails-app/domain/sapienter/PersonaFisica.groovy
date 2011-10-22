a5ada9c72daec445e29913f70e2be194406ec013
nicolassouto <nicolassouto@gmail.com>
2011-10-22 21:38:17 +0000

    Solución de reportes

--- Sapienter/grails-app/domain/sapienter/PersonaFisica.groovy
+++ Sapienter/grails-app/domain/sapienter/PersonaFisica.groovy
@@ -5,8 +5,8 @@ import ar.com.fdvs.dj.domain.AutoText
 
 class PersonaFisica extends Persona{
 	def static reportable = [
-		columns: ['apellido', 'nombre', 'dni', 'fechanacimiento', 'email'],
-		columnTitles: ['apellido': 'Apellido', 'nombre': 'Nombre', 'dni': 'DNI', 'fechanacimiento':'Fecha de Nacimiento', 'email': 'E-mail'],
+		columns: ['apellido', 'nombre', 'dni', 'fechaNacimiento', 'email'],
+		columnTitles: ['apellido': 'Apellido', 'nombre': 'Nombre', 'dni': 'DNI', 'fechaNacimiento':'Fecha de Nacimiento', 'email': 'E-mail'],
 		autoTexts: [new AutoText(AutoText.AUTOTEXT_PAGE_X_OF_Y, AutoText.POSITION_FOOTER, HorizontalBandAlignment.buildAligment(AutoText.ALIGMENT_CENTER), (byte)0, 200, 200)]]
 	static searchable = true
 	String apellido
