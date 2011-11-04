8b3aaf2c507653d9b8fbc284083aaf1b49dc7dce
gbonsoir@gmail.com <gbonsoir@gmail.com@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-11-04 21:28:39 +0000

    Algunos cambios en el bootstrap para ir preparando la carga inicial con datos que se vean bien

--- trunk/Sapienter/grails-app/conf/BootStrap.groovy
+++ trunk/Sapienter/grails-app/conf/BootStrap.groovy
@@ -17,41 +17,8 @@ import sapienter.CorreoElectronico
 class BootStrap {
 
 	def init = { servletContext ->
-		def est = new Estudio(razonSocial: "Sapienter", cuit: "20-30409317-0", domicilioFiscal: "Mozart y Zaraza")
-		est.save()
-		if (est.hasErrors()) {
-			println est.errors
-		}
-		def perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Matias", apellido: "Stani", dni: "31111111", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
-		perFis.save()
-		if (perFis.hasErrors()) {
-			println perFis.errors
-		}		
-		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Nicolas", apellido: "Souto", dni:"30000000", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
-		perFis.save()
-		if (perFis.hasErrors()) {
-			println perFis.errors
-		}
-		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Romina", apellido: "Perrone", dni: "3000001", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
-		perFis.save()
-		if (perFis.hasErrors()) {
-			println perFis.errors
-		}
-		def perJur = new PersonaJuridica(razonSocial:"Matute SA", cuit:"4343242", email:"matute@gmail.com",telefonoDeContacto:"5443", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
-		perJur.save()
-		if (perJur.hasErrors()) {
-			println perJur.errors
-		}
-		perJur = new PersonaJuridica(razonSocial:"Nicolote SA", cuit:"4332247", email:"nicolote@gmail.com",telefonoDeContacto:"45", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
-		perJur.save()
-		if (perJur.hasErrors()) {
-			println perJur.errors
-		}
-		perJur = new PersonaJuridica(razonSocial:"Romain SA", cuit:"4122247", email:"romain@gmail.com",telefonoDeContacto:"23", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
-		perJur.save()
-		if (perJur.hasErrors()) {
-			println perJur.errors
-		}
+		
+		/* Categorias */
 		def cat = new Categoria(nombreCategoria:"Judicial")
 		cat.save()
 		if (cat.hasErrors()) {
@@ -63,7 +30,60 @@ class BootStrap {
 				println cat.errors
 		}
 		
-		def juzgado = new Juzgado(estudio: (Estudio.getAll().get(0)),nombreDeJuzgado: "Juzgado 1", numeroDeJuzgado:"1", direccionJuzgado: "AV. DE LOS INMIGRANTES 1950 PISO 4°")
+		/* Sub Categorias */
+		def subCat = new Subcategoria(subTipoCategoria:"Postulatoria", categoria:(Categoria.findByNombreCategoria("Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"Probatoria", categoria:(Categoria.findByNombreCategoria("Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"Conclusiva", categoria:(Categoria.findByNombreCategoria("Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"De Resolución", categoria:(Categoria.findByNombreCategoria("Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"Impugnitiva", categoria:(Categoria.findByNombreCategoria("Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"Extra-Judicial", categoria:(Categoria.findByNombreCategoria("Extra-Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		subCat = new Subcategoria(subTipoCategoria:"Mediación", categoria:(Categoria.findByNombreCategoria("Extra-Judicial")))
+		subCat.save()
+		if (subCat.hasErrors()){
+			println subCat.errors
+		}
+		
+		/* Estudio */
+		def est = new Estudio(razonSocial: "Sapienter", cuit: "20-30409317-0", domicilioFiscal: "Av. Cordoba 600")
+		est.save()
+		if (est.hasErrors()) {
+			println est.errors
+		}
+		/* Modelos de documentos */
+		/* TODO cargar */
+		
+		/* Enlace Util */
+		/* TODO cargar */
+		
+		/* Jurisprudencia */
+		/* TODO cargar */
+		
+		/* Juzgados */
+		def juzgado = new Juzgado(estudio: (Estudio.getAll().get(0)), nombreDeJuzgado: "Juzgado 1", numeroDeJuzgado:"1", direccionJuzgado: "AV. DE LOS INMIGRANTES 1950 PISO 4°")
 		juzgado.save()
 		if (juzgado.hasErrors()){
 			println juzgado.errors
@@ -105,42 +125,7 @@ class BootStrap {
 			println juzgado.errors
 		}
 
-				
-		def subCat = new Subcategoria(subTipoCategoria:"Postulatoria", categoria:(Categoria.findByNombreCategoria("Judicial")))
-        	subCat.save()
-			if (subCat.hasErrors()){
-				println subCat.errors
-			}
-		subCat = new Subcategoria(subTipoCategoria:"Probatoria", categoria:(Categoria.findByNombreCategoria("Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
-		subCat = new Subcategoria(subTipoCategoria:"Conclusiva", categoria:(Categoria.findByNombreCategoria("Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
-		subCat = new Subcategoria(subTipoCategoria:"De Resolución", categoria:(Categoria.findByNombreCategoria("Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
-		subCat = new Subcategoria(subTipoCategoria:"Impugnitiva", categoria:(Categoria.findByNombreCategoria("Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
-		subCat = new Subcategoria(subTipoCategoria:"Extra-Judicial", categoria:(Categoria.findByNombreCategoria("Extra-Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
-		subCat = new Subcategoria(subTipoCategoria:"Mediación", categoria:(Categoria.findByNombreCategoria("Extra-Judicial")))
-		subCat.save()
-		if (subCat.hasErrors()){
-			println subCat.errors
-		}
+		/* Tipo de parte */
 		def tipPar
 		tipPar = new TipoDeParte(descripcion:"Contraparte", estudio: (Estudio.getAll().get(0)))
 		tipPar.save()
@@ -152,8 +137,8 @@ class BootStrap {
 		if (tipPar.hasErrors()){
 			println tipPar.errors
 		}
-
-		tipPar = new TipoDeParte(descripcion:"Testigo", estudio: (Estudio.getAll().get(0))) 
+		
+		tipPar = new TipoDeParte(descripcion:"Testigo", estudio: (Estudio.getAll().get(0)))
 		tipPar.save()
 		if (tipPar.hasErrors()){
 			println tipPar.errors
@@ -164,7 +149,7 @@ class BootStrap {
 		if (tipPar.hasErrors()){
 			println tipPar.errors
 		}
-
+		
 		tipPar = new TipoDeParte(descripcion:"Juez", estudio: (Estudio.getAll().get(0)))
 		tipPar.save()
 		if (tipPar.hasErrors()){
@@ -175,10 +160,24 @@ class BootStrap {
 		if (tipPar.hasErrors()){
 			println tipPar.errors
 		}
-		def calendar =new Calendario()
+		
+		
+
+		
+		
+		/* Roles*/		
 		def jrRole = SecRole.findByAuthority('ROLE_JUNIOR') ?: new SecRole(authority: 'ROLE_JUNIOR').save(failOnError: true)
 		def ssrRole = SecRole.findByAuthority('ROLE_SSR') ?: new SecRole(authority: 'ROLE_SSR').save(failOnError: true)
 		def srRole = SecRole.findByAuthority('ROLE_SENIOR') ?: new SecRole(authority: 'ROLE_SENIOR').save(failOnError: true)
+		
+		/* Usuarios con su calendario y correo electronico */
+		def calendar =new Calendario()
+		def correoElec = new CorreoElectronico(
+			contrasenia: "Sapienter01!",
+			direccion: "sapienterTest@gmail.com", //replace with %40 for imap
+			smtpUrl: "smtp.gmail.com",
+			imapUrl: "imap.gmail.com:993/inbox")
+		correoElec.save()
 		def adminUser = SecUser.findByUsername('admin') ?: new Usuario(
 			username: 'admin',
 			password: 'admin',
@@ -186,33 +185,7 @@ class BootStrap {
 			apellido: 'Stanislavsky',
 			calendario: calendar, 
 			estudio: (Estudio.getAll().get(0)),
-			comentarios:'Nada en particular',
-			telefono:'4354242',
-			rol:srRole,
-			enabled: true).save(failOnError: true)
- 
-		if (!adminUser.authorities.contains(srRole)) {
-			SecUserSecRole.create adminUser, srRole
-		}
-		
-		def correoElec = new CorreoElectronico(
-				contrasenia: "Sapienter01!",
-				direccion: "sapienterTest@gmail.com", //replace with %40 for imap
-				smtpUrl: "smtp.gmail.com",
-				imapUrl: "imap.gmail.com:993/inbox")
-		correoElec.save()
-		if (correoElec.hasErrors()) {
-			println est.errors
-		}
-		
-		def mailUser = SecUser.findByUsername('gbonsoir') ?: new Usuario(
-			username: 'gbonsoir',
-			password: 'gbonsoir',
-			nombre: 'Gabriel',
-			apellido: 'Bonsoir',
-			calendario: calendar,
-			estudio: (Estudio.getAll().get(0)),
-			comentarios:'Para probar E Mail',
+			comentarios:'Administrador del sistema',
 			telefono:'4354242',
 			rol:srRole,
 			correoElectronico: (CorreoElectronico.findByDireccion('sapienterTest@gmail.com')),
@@ -221,7 +194,70 @@ class BootStrap {
 		if (!adminUser.authorities.contains(srRole)) {
 			SecUserSecRole.create adminUser, srRole
 		}
+		
+		correoElec = new CorreoElectronico(
+				contrasenia: "Sapienter01!",
+				direccion: "sapienterTest@gmail.com", //replace with %40 for imap
+				smtpUrl: "smtp.gmail.com",
+				imapUrl: "imap.gmail.com:993/inbox")
+		correoElec.save()
+		if (correoElec.hasErrors()) {
+			println est.errors
+		}
+		calendar =new Calendario()
+		def mailUser = SecUser.findByUsername('gbonsoir') ?: new Usuario(
+			username: 'gbonsoir',
+			password: 'gbonsoir',
+			nombre: 'Gabriel',
+			apellido: 'Bonsoir',
+			calendario: calendar,
+			estudio: (Estudio.getAll().get(0)),
+			comentarios:'Usuario Senior',
+			telefono:'4435-4242',
+			rol:srRole,
+			correoElectronico: (CorreoElectronico.findByDireccion('sapienterTest@gmail.com')),
+			enabled: true).save(failOnError: true)
+ 
+		if (!adminUser.authorities.contains(srRole)) {
+			SecUserSecRole.create adminUser, srRole
+		}
+		
+		
+		/* Personas Fisicas - Sin procesos */
+		def perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Contador",fax: "",telefono:"4107-7326",codPostal: "1003",ciudad:"CABA",domicilio:"San Martin 344",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Gabriel", apellido: "Bonsoir", dni: "27123985", pais: "Argentina", email: "gbonsoir@sapienter.org.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Nicolas", apellido: "Souto", dni:"30000000", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Romina", apellido: "Perrone", dni: "3000001", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		/* Personas Juridicas */
+		def perJur = new PersonaJuridica(razonSocial:"Matute SA", cuit:"4343242", email:"matute@gmail.com",telefonoDeContacto:"5443", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Nicolote SA", cuit:"4332247", email:"nicolote@gmail.com",telefonoDeContacto:"45", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Romain SA", cuit:"4122247", email:"romain@gmail.com",telefonoDeContacto:"23", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		
 	}
+	
 	def destroy = {
 	}
 }
