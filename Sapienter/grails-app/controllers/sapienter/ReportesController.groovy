a900f6694476c0cddeffaff5a53a23b77556897d
romi.perrone <romi.perrone@gmail.com>
2011-11-06 00:35:16 +0000

    Reportes!!!

---
+++ Sapienter/grails-app/controllers/sapienter/ReportesController.groovy
@@ -0,0 +1,16 @@
+package sapienter
+
+import grails.plugins.springsecurity.Secured
+
+class ReportesController {
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+    def index = {
+		redirect(action: "list", params: params)
+	}
+	
+	@Secured(['IS_AUTHENTICATED_FULLY'])
+	def list = {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[params:params]
+	}
+}
