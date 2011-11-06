45dbe4611b4700601a19bc73f400ca48b0296496
nicolassouto <nicolassouto@gmail.com>
2011-11-06 18:35:49 +0000

    Validacion en parte

--- Sapienter/grails-app/domain/sapienter/Parte.groovy
+++ Sapienter/grails-app/domain/sapienter/Parte.groovy
@@ -7,10 +7,31 @@ class Parte {
 	Persona persona
 	TipoDeParte tipo
     static constraints = {
-		Apellido()
-		Nombre()
+		Apellido(validator: { val, obj ->
+				def oneOrTheOther = true
+				if ((val != null || obj.Nombre != null) &&
+					obj.persona != null)
+				oneOrTheOther = false
+				return oneOrTheOther
+        })
+		Nombre(validator: { val, obj ->
+				def oneOrTheOther = true
+				if ((obj.Nombre != null || val != null) &&
+					obj.persona != null)
+				oneOrTheOther = false
+				return oneOrTheOther
+        })
 		tipo()
-		persona(nullable:true)
+		persona(nullable:true,validator: { val, obj ->
+			def oneOrTheOther = true
+			if (
+				
+				(  (obj.Apellido != null && obj.Apellido != "")
+				|| (obj.Nombre != null   && obj.Nombre != "") ) 
+				&& (val != null && val != ""))
+				oneOrTheOther = false
+			return oneOrTheOther
+        })
     }
 	String toString(){
 		tipo
