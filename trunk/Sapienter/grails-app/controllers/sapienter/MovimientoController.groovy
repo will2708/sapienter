6f5230dcf1fc575f4ef47f4c482248adfa00aa6e
matias.toth@gmail.com <matias.toth@gmail.com@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-11-07 04:27:56 +0000

    Workflow working 4

--- trunk/Sapienter/grails-app/controllers/sapienter/MovimientoController.groovy
+++ trunk/Sapienter/grails-app/controllers/sapienter/MovimientoController.groovy
@@ -19,9 +19,18 @@ class MovimientoController {
 		def proceso1 = Proceso.get(params.proceso.id)
 		def user = SecUser.get(springSecurityService.principal.id)
 		def srRole = SecRole.findByAuthority('ROLE_SENIOR')
-		if (user.rol != srRole) {
+		def userAut = false
+		if (user.role != srRole) {
+			
 			if (proceso1.usuariosAutorizados != null) {
-				if (proceso1.usuariosAutorizados.contains(user)){
+				for (Iterator iterator = proceso1.usuariosAutorizados.iterator(); iterator
+						.hasNext();) {
+					SecUser users = (SecUser) iterator.next();
+					if (users.id == user.id)
+						userAut = true										
+				}
+				
+				if (userAut){
 				}
 				else {
 					flash.message = "${message(code: 'default.not.authorized.message')}"
@@ -37,12 +46,23 @@ class MovimientoController {
     }
 	@Secured(['IS_AUTHENTICATED_FULLY'])
     def save = {
-		def proceso1 = params.proceso
+		def map = params
+		def test = params
+		def proceso1 = Proceso.get(params.proceso)
 		def user = SecUser.get(springSecurityService.principal.id)
 		def srRole = SecRole.findByAuthority('ROLE_SENIOR')
-		if (user.rol != srRole) {
-			if (proceso.usuariosAutorizados != null) {
-				if (proceso.usuariosAutorizados.contains(user)){
+		def userAut = false
+		if (user.role != srRole) {
+			
+			if (proceso1.usuariosAutorizados != null) {
+				for (Iterator iterator = proceso1.usuariosAutorizados.iterator(); iterator
+						.hasNext();) {
+					SecUser users = (SecUser) iterator.next();
+					if (users.id == user.id)
+						userAut = true										
+				}
+				
+				if (userAut){
 				}
 				else {
 					flash.message = "${message(code: 'default.not.authorized.message')}"
