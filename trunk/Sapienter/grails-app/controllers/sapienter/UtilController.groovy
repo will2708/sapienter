7e95754a4ad84cb63e682f7a00a45a19cfad59af
gbonsoir@gmail.com <gbonsoir@gmail.com@d8be9e6c-d5fb-c119-c762-baa9bde600a8>
2011-11-07 21:02:01 +0000

    Notificaicón en la barra.

---
+++ trunk/Sapienter/grails-app/controllers/sapienter/UtilController.groovy
@@ -0,0 +1,23 @@
+package sapienter
+
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+class UtilController {
+
+    def index = { }
+	
+	def alert = {
+		
+		def mailStatus = ApplicationHolder.application.mainContext.getBean("mailStatus")
+		if( (mailStatus == null) || mailStatus?.status == null)
+			mailStatus.status="noCurrent"
+//		render "${mailStatus.status}" // Just renders the User count and we are done !!!
+
+		if(mailStatus.status == "current")
+			render "<img class=\"image\" src=\"${createLinkTo(dir: 'images/', file: 'email13.png')}\"/>"
+		
+		else
+			render "<img class=\"image\" src=\"${createLinkTo(dir: 'images/', file: 'email_envelope.png')}\"/>"
+			
+//		render "${Categoria.count()}"+mailStatus.status
+	}
+}
