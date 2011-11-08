bf07179fbd3da4d56e143c30e939f19c351d1ba3
romi.perrone <romi.perrone@gmail.com>
2011-11-08 18:11:51 +0000

    saco icepush. Casi no se usaba y evitamos mensaje de error en varias pantallas

---
+++ Sapienter/grails-app/taglib/sapienter/NotificacionMailTagLib.groovy
@@ -0,0 +1,22 @@
+package sapienter
+
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+class NotificacionMailTagLib {
+
+	/**
+	* Returns images depending if there are pending e-mails
+	*/
+	def notificacionMail = {
+		def mailStatus = ApplicationHolder.application.mainContext.getBean("mailStatus")
+		if( (mailStatus == null) || mailStatus?.status == null){
+			mailStatus.status="noCurrent"
+			//		render "${mailStatus.status}" // Just renders the User count and we are done !!!
+		}
+		if(mailStatus.status == "current"){
+			out << "<img class=\"image\" src=\"${createLinkTo(dir: 'images/', file: 'email18.png')}\"/>"
+		}
+		else{
+			out << "<img class=\"image\" src=\"${createLinkTo(dir: 'images/', file: 'email13.png')}\"/>"
+		}
+	}
+}
