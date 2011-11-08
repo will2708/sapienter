491b2c9e0f08eb7ae99594aae48866c31c100c22
romi.perrone <romi.perrone@gmail.com>
2011-11-08 00:36:17 +0000

    borre id de usuario css

--- Sapienter/grails-app/conf/BootStrap.groovy
+++ Sapienter/grails-app/conf/BootStrap.groovy
@@ -162,38 +162,107 @@ class BootStrap {
 		}
 		
 		/* Personas Fisicas - Sin procesos */
-		def perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Contador",fax: "",telefono:"4107-7326",codPostal: "1003",ciudad:"CABA",domicilio:"San Martin 344",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Gabriel", apellido: "Bonsoir", dni: "27123985", pais: "Argentina", email: "gbonsoir@sapienter.org.com", estudio: (Estudio.getAll().get(0)))
+		def perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Contador",fax: "",telefono:"4107-7326",codPostal: "1003",ciudad:"CABA",domicilio:"San Martin 344",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Gabriel", apellido: "Bonsoir", dni: "26.624.132", pais: "Argentina", email: "gbonsoir@sapienter.org", estudio: (Estudio.getAll().get(0)))
 		perFis.save()
 		if (perFis.hasErrors()) {
 			println perFis.errors
 		}
-		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Nicolas", apellido: "Souto", dni:"30000000", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"15462-8989",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Nicolas", apellido: "Souto", dni:"32.456.777", pais: "Argentina", email: "nsouto@sapienter.org", estudio: (Estudio.getAll().get(0)))
 		perFis.save()
 		if (perFis.hasErrors()) {
 			println perFis.errors
 		}
-		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "",fax: "",telefono:"",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Romina", apellido: "Perrone", dni: "3000001", pais: "Argentina", email: "a@a.com", estudio: (Estudio.getAll().get(0)))
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Analista de sistemas",fax: "",telefono:"155376-9878",codPostal: "",ciudad:"",domicilio:"",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Romina", apellido: "Perrone", dni: "32.677.644", pais: "Argentina", email: "rperrone@sapienter.org", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Diseñadora Gráfica",fax: "",telefono:"4855-2424",codPostal: "1224",ciudad:"CABA",domicilio:"Sarmiento 3208 PB 2",conyuge:"Germán Camardella",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Mailen", apellido: "Brandao", dni: "32.675.888", pais: "Argentina", email: "mailen.brandaor@gmail.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Arquitecta",fax: "",telefono:"4958-2934",codPostal: "1224",ciudad:"CABA",domicilio:"Agrelo 4050",conyuge:"",madre: "", padre: "",estadoCivil: "Casado", fechaNacimiento: new Date(),observaciones:"", nombre: "Manuela", apellido: "Bresso", dni: "14.676.888", pais: "Argentina", email: "manuela_bresso@yahoo.com.ar", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Docente",fax: "",telefono:"4637-7267",codPostal: "1437",ciudad:"CABA",domicilio:"Varela 887 1°D",conyuge:"",madre: "", padre: "",estadoCivil: "Casado", fechaNacimiento: new Date(),observaciones:"", nombre: "Alejandro", apellido: "Sanchez", dni: "11.632.765", pais: "Argentina", email: "alejandro_sanchez@hotmail.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Ingeniero",fax: "",telefono:"4456-5656",codPostal: "1433",ciudad:"CABA",domicilio:"Matheu 564 4°A",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Fabian", apellido: "Ippolito", dni: "28.712.111", pais: "Argentina", email: "fabian.ippolito@gmail.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Fonaudiologa",fax: "",telefono:"4456-8798",codPostal: "1433",ciudad:"CABA",domicilio:"Alberti 1632",conyuge:"",madre: "", padre: "",estadoCivil: "Soltero", fechaNacimiento: new Date(),observaciones:"", nombre: "Natalia", apellido: "Ippolito", dni: "32.564.878", pais: "Argentina", email: "nataliaippolito@gmail.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Docente",fax: "",telefono:"4982-4444",codPostal: "1424",ciudad:"CABA",domicilio:"Rosario 78 6°B",conyuge:"",madre: "", padre: "",estadoCivil: "Divorciada", fechaNacimiento: new Date(),observaciones:"", nombre: "Susana", apellido: "Muñoz", dni: "11.632.444", pais: "Argentina", email: "susana_muñoz@hotmail.com", estudio: (Estudio.getAll().get(0)))
+		perFis.save()
+		if (perFis.hasErrors()) {
+			println perFis.errors
+		}
+		perFis = new PersonaFisica(auxiliar:"",informacionTributaria:"",profesion: "Carpintero",fax: "",telefono:"4432-8877",codPostal: "1423",ciudad:"CABA",domicilio:"Pedernera 897",conyuge:"",madre: "", padre: "",estadoCivil: "Viudo", fechaNacimiento: new Date(),observaciones:"", nombre: "Marcelo", apellido: "Colagiovani", dni: "23.787.898", pais: "Argentina", email: "susana_muñoz@hotmail.com", estudio: (Estudio.getAll().get(0)))
 		perFis.save()
 		if (perFis.hasErrors()) {
 			println perFis.errors
 		}
 		/* Personas Juridicas */
-		def perJur = new PersonaJuridica(razonSocial:"Matute SA", cuit:"4343242", email:"matute@gmail.com",telefonoDeContacto:"5443", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		def perJur = new PersonaJuridica(razonSocial:"Matute SA", cuit:"27-564777-3", email:"matute@gmail.com",telefonoDeContacto:"5443-9898", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
 		perJur.save()
 		if (perJur.hasErrors()) {
 			println perJur.errors
 		}
-		perJur = new PersonaJuridica(razonSocial:"Nicolote SA", cuit:"4332247", email:"nicolote@gmail.com",telefonoDeContacto:"45", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur = new PersonaJuridica(razonSocial:"Nicolote SA", cuit:"27-654888-5", email:"nicolote@gmail.com",telefonoDeContacto:"4566-8787", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
 		perJur.save()
 		if (perJur.hasErrors()) {
 			println perJur.errors
 		}
-		perJur = new PersonaJuridica(razonSocial:"Romain SA", cuit:"4122247", email:"romain@gmail.com",telefonoDeContacto:"23", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur = new PersonaJuridica(razonSocial:"Romain SA", cuit:"27-876654-8", email:"romain@gmail.com",telefonoDeContacto:"4562-8983", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
 		perJur.save()
 		if (perJur.hasErrors()) {
 			println perJur.errors
 		}
-		
+		perJur = new PersonaJuridica(razonSocial:"Hexastar SA", cuit:"27-999788-9", email:"info@hexastar.com",telefonoDeContacto:"4308-9999", auxiliares:"", fax:"", observaciones:"Contactar a Fernando Salgado", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Capgemini SA", cuit:"27-334765-9", email:"legales@capgemini.com",telefonoDeContacto:"4785-8093", auxiliares:"", fax:"4785-8090", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Kimberly Clark", cuit:"27-324555-6", email:"luciana.castaño@kimberly-clark.com",telefonoDeContacto:"4308-9999", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"DL Consultores SRL", cuit:"27-234111-6", email:"matias-trumb@dlconsultores.com.ar",telefonoDeContacto:"4983-4545", auxiliares:"", fax:"4983-4555", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Captton", cuit:"27-765999-3", email:"matias.micenm@captton.com",telefonoDeContacto:"4922-6767", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Farmacity", cuit:"27-876987-1", email:"farmacity@farmacity.com",telefonoDeContacto:"4896-5556", auxiliares:"", fax:"", observaciones:"Contactar a Martín Melidoni", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
+		perJur = new PersonaJuridica(razonSocial:"Femsa SA", cuit:"27-324556-2", email:"legales@coca-cola.com.ar",telefonoDeContacto:"4922-6767", auxiliares:"", fax:"", observaciones:"", estudio: (Estudio.getAll().get(0)))
+		perJur.save()
+		if (perJur.hasErrors()) {
+			println perJur.errors
+		}
 		createUsersAndGroups()
 	}
 	
